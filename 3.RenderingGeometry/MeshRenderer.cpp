cf6e53d51c0051ce2cc4db28f1f4ccff404638b7
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2018-10-04 12:27:02 -0500

    added texture coodinate argument

--- 3.RenderingGeometry/MeshRenderer.cpp
+++ 3.RenderingGeometry/MeshRenderer.cpp
@@ -68,6 +68,9 @@ int MeshRenderer::create_buffers()
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(glm::vec4)));
 
+	//textures
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)32);
 
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
